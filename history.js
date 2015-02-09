b3aec5ffd1fd1e7bc88b36598fe545885d371ac1
Mark Tonkinson <DHTMaster@gmail.com>
2015-02-09 11:27:26 -0700

    Progress made on the history page now.  The table is displaying the data, however, I'm trying to use bracket notation to select a an object property- dot notation works, when I use bracket with the actual property in quotes it works, but when I pass it the variable, it doesn't work. . . i've checked to make sure the variable is a string, is there something I don't know about jquery and bracket notation?

--- history.js
+++ history.js
@@ -3,48 +3,65 @@ var dinnerPartyRef = new Firebase("https://tonkinsondinnerparty.firebaseio.com/"
 
 
 	var getRef = new Firebase(dinnerPartyRef + "/history")
+	var counterRef = new Firebase(dinnerPartyRef + '/counter');
+	var historyRef = new Firebase(dinnerPartyRef + '/history/');
+	var counter;
+		//get counter value for history- this needs to happen first- and also only needs to happen when it's updated
+	var once = function(){
+		counterRef.once("value", function(snapshot){
+			counter = snapshot.val();
+		}, function(errorObject){
+			console.log("the read failed:" + errorObject.code)
+		})
+	}
+	once()
 
 	getRef.on("value", function(snapshot) {
 		var history = snapshot.val()
 		
 		var historyLength = Object.keys(history).length
-
+		$('#dynamic-history').append('<table></table>');
 		for(var i = 1; i <= historyLength; i++){
-			$('#dynamic-history').append('<table></table>');
+			
 			var newTable = $('#dynamic-history').children()
 			var dinner = "dinner" + i
 			
 			console.log(history[dinner])
-			//could reduce code here by changing the tags to a variable
-			//var open = '<tr><td>'
-			newTable.append('<tr><td> Dinner Theme : ' + history[dinner].dinnerTheme + '</td><td> First Course : ' + history[dinner].firstCourse + '</tr></td>')
+			//could reduce code here by changing the tags to a variable- but would that add much?
+			//var openTag = '<tr><td> '
+			//table head doesn't work here - because it's at the top of a table
+			newTable.append('<tr class="history-title"><td> Dinner' + i + ' <input type="submit" val=' + dinner + ' placeholder="Set To Current Dinner"></td></tr>')
+			newTable.append('<tr><td> Dinner Theme  ' + history[dinner].dinnerTheme + '</td><td> First Course : ' + history[dinner].firstCourse + '</tr></td>')
 			newTable.append('<tr><td> Location : ' + history[dinner].location + '</td><td> Second Course : ' + history[dinner].secondCourse + '</td></tr>')
-			newTable.append('<tr><td> Time : ' + history[dinner].time + '<td> Third Course : ' + history[dinner].thirdCourse + '</td></tr>')
-			newTable.append('<tr><td> Date : ' + history[dinner].date + '<td> Fourth Course : ' + history[dinner].fourthCourse + '</td>')
-			newTable.append('<tr><td> Price : ' + history[dinner].price + '</td></tr>')
-		
-			
-			newTable.append('<tr><td> Spots Available : ' + history[dinner].spotsAvailable + '</td></tr>')
-			newTable.append('<tr><td> Attendees : ' + history[dinner].attendees + '</td></tr>')
-
-
-			
-
+			newTable.append('<tr><td> Time : ' + history[dinner].time + '</td><td> Third Course : ' + history[dinner].thirdCourse + '</td></tr>')
+			newTable.append('<tr><td> Date : ' + history[dinner].date + '</td><td> Fourth Course : ' + history[dinner].fourthCourse + '</td></tr>')
+			newTable.append('<tr><td> Price : ' + history[dinner].price + '</td><td> Attended : ' + history[dinner].attendees + '/' + history[dinner].spotsAvailable + '</td></tr>')
+			newTable.append('<tr class="invisible-row"><td></td><td></td></tr>')		
 		}
+		
+		//what is the difference- why does the variable return undefined?
+		$(".history-title").on('click', function(){
+			var dinner2 = $(this).text() //returns as Dinner + num
+			dinner2 = dinner2.replace('D','d')
+			console.log(dinner2)
+			var historyTwo = history;
+			console.log(historyTwo)
+			console.log(historyTwo.dinner1)
+			console.log(historyTwo['dinner1'])
+			console.log(dinner2)
+			console.log(historyTwo[dinner2])
 
+			// dinnerPartyRef.update({currentDinner: history[dinner]})
+			// var dinnerNum = "dinner" + (counter.value += 1)
+			// historyRef.child(dinnerNum).set(history[dinner])
+			// counterRef.set(counter)
+		})
 
-
-	  // var partyInfo = snapshot.val();
-	  // $('#dinner-theme').text(partyInfo.dinnerTheme);
-	  // $('#dinner-location').text(partyInfo.location)
-	  // $('#dinner-time').text(partyInfo.time)
-	  // $('#dinner-price').text(partyInfo.price)
-	  // $('#first-course').text(partyInfo.firstCourse)
-	  // $('#second-course').text(partyInfo.secondCourse)
-	  // $('#third-course').text(partyInfo.thirdCourse)
-	  // $('#fourth-course').text(partyInfo.fourthCourse)
-	  // $('#spots-left').text(partyInfo.spotsAvailable - partyInfo.attendees + '/' + partyInfo.spotsAvailable)
 	}, function (errorObject) {
 	  console.log("The read failed: " + errorObject.code);
 	});
+
+	
+
+	
 
--- planning.js
+++ planning.js
@@ -2,98 +2,33 @@ $(document).ready(function(){
 	
 	var dinnerPartyRef = new Firebase("https://tonkinsondinnerparty.firebaseio.com/");
 	var counterRef = new Firebase(dinnerPartyRef + '/counter');
-	
 	var historyRef = new Firebase(dinnerPartyRef + '/history/');
-	var obj = {}
 	var counter;
 	
 
 
 	var newDinner = function(dinnerObj){
-		//this is always the case, no need to have to input it
-		//console.log(dinnerObj)
-		dinnerObj.attendees = 0
-		// var DinnerCurrent = { 
-		// 		dinnerTheme: "The Great Wall Feasting",
-		// 		location: "Tonkinson Apartment",
-		// 		time: "7pm",
-		// 		date: "January 27",
-		// 		firstCourse: "Boadza",
-		// 		secondCourse: "Mandarin Chicken Orange Salad or Peanut Dressing Salad",
-		// 		thirdCourse: "General Tzos and Orange Chicken with fried rice and steam vegetables",
-		// 		fourthCourse: "Dessert- to be determined",
-		// 		price: "$8.00",
-		// 		spotsAvailable: 6,
-		// 		attendees: 0
-		// 	}
+		var obj = {}
+		var dinnerNum = "dinner" + (counter.value += 1)
 		
-		
-		var p = "dinner" + (counter.value + 1)
-		// //console.log(p)
-		obj = dinnerObj
-		//console.log(obj)
-		// //its breaking here- it doesn't generate a new endpoint even though I'm giving it a new name- it overwrites the old one
-		//dinnerPartyRef.push(dinnerObj)
-		
-		historyRef.child(p).set(obj,
-			// dinner2: {
-			// 	dinnerTheme: "The Great Wall Feasting",
-			// 	location: "Tonkinson Apartment",
-			// 	time: "7pm",
-			// 	date: "January 27",
-			// 	firstCourse: "Boadza",
-			// 	secondCourse: "Mandarin Chicken Orange Salad or Peanut Dressing Salad",
-			// 	thirdCourse: "General Tzos and Orange Chicken with fried rice and steam vegetables",
-			// 	fourthCourse: "Dessert- to be determined",
-			// 	price: "$8.00",
-			// 	spotsAvailable: 6,
-			// 	attendees: 0}
-			// }, 
-			function(error, success){
+		historyRef.child(dinnerNum).set(dinnerObj,
+
+			function(error){
 			if(error){
 				console.log(error)
-			} else if (success){
-				console.log('success', success);
-			} else {
-				console.log("i guess not")
 			}
 		})
-
-		// newRef.child('dinner2').set(obj, function(error, success){
-		// 	if(error){
-		// 		console.log(error)
-		// 	} else if (success){
-		// 		console.log('success', success);
-		// 	} else {
-		// 		console.log("i guess not")
-		// 	}
-		// })
-
-		// newRef.child(p).setWithPriority(obj, counter.value, function(error, success){
-		// 	if(error){
-		// 		console.log(error)
-		// 	} else if (success){
-		// 		console.log('success', success);
-		// 	} else {
-		// 		console.log("i guess not")
-		// 	}
-		// })
-		//.set overwrites all children at the location. which answers the problem above
-		//newHistoryRef.set(obj[p])
-		counter.value +=1
 		
 		counterRef.set(counter)
 
 	}
-	//newDinner()
+	
 
 
-	//get counter value for history- this needs to happen first
+	//get counter value for history- this needs to happen first- and also only needs to happen when it's updated
 	var once = function(){
 		counterRef.once("value", function(snapshot){
 			counter = snapshot.val();
-			console.log(counter)
-			//newDinner();
 		}, function(errorObject){
 			console.log("the read failed:" + errorObject.code)
 		})
@@ -101,12 +36,7 @@ $(document).ready(function(){
 	once()
 
 	
-	
-	
-
-
-	
-	
+	//get the values from the form
 
 	$("form").submit(function(event){
 
@@ -122,8 +52,8 @@ $(document).ready(function(){
 			fourthCourse: $('#planned-course4').val(),
 			spotsAvailable: $('#spots-available').val()
 		}
-		console.log(DinnerCurrent.date)
-		
+
+		DinnerCurrent.attendees = '0';
 		event.preventDefault();
 		dinnerPartyRef.update({currentDinner: DinnerCurrent}, function(err){
 			if(err){
@@ -132,8 +62,6 @@ $(document).ready(function(){
 				newDinner(DinnerCurrent);
 			}
 		})
-		
-		//newDinner(DinnerCurrent);
 		
 	})
 
